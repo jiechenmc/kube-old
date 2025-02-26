759cd704e82efc6d2ec856276d8c0f11ab27b9bb
Jie Chen <jiechenmc@gmail.com>
2025-02-25 22:24:01 -0500

    add workflow

--- main.go
+++ main.go
@@ -1,12 +1,7 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"github.com/jackc/pgx/v5"
-	"log"
 	"net/http"
-	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,39 +9,40 @@ import (
 func main() {
 
 	// Start Up
-	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-		os.Exit(1)
-	}
-	defer conn.Close(context.Background())
+	// conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	// if err != nil {
+	// 	fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+	// 	os.Exit(1)
+	// }
+	// defer conn.Close(context.Background())
 
 	r := gin.Default()
 
-	r.POST("/", func(c *gin.Context) {
-		// insert SQL
-		upsertSQL := `
-		INSERT INTO registry (name, URI)
-		VALUES ($1, $2)
-		`
-
-		// Example data to upsert
-		name := "example_name"
-		URI := "https://example.com"
-
-		// Execute the upsert
-		_, err = conn.Exec(context.Background(), upsertSQL, name, URI)
-		if err != nil {
-			log.Fatalf("Failed to upsert data: %v\n", err)
-		}
-
-		c.JSON(http.StatusOK, gin.H{
-			"name": name,
-			"uri":  URI,
-		})
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"Connected": true})
 	})
-	err = r.Run()
-	if err != nil {
-		return
-	}
+	// r.POST("/", func(c *gin.Context) {
+	// 	// insert SQL
+	// 	upsertSQL := `
+	// 	INSERT INTO registry (name, URI)
+	// 	VALUES ($1, $2)
+	// 	`
+
+	// 	// Example data to upsert
+	// 	name := "example_name"
+	// 	URI := "https://example.com"
+
+	// 	// Execute the upsert
+	// 	_, err = conn.Exec(context.Background(), upsertSQL, name, URI)
+	// 	if err != nil {
+	// 		log.Fatalf("Failed to upsert data: %v\n", err)
+	// 	}
+
+	// 	c.JSON(http.StatusOK, gin.H{
+	// 		"name": name,
+	// 		"uri":  URI,
+	// 	})
+	// })
+	r.Run()
+
 }
