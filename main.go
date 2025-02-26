8f9af46f1d25fdb47c9a8473f8ee48caf56c8026
Jie Chen <jiechenmc@gmail.com>
2025-02-26 11:05:01 -0500

    htmx is amazing

--- main.go
+++ main.go
@@ -10,12 +10,22 @@ import (
 func main() {
 
 	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
 
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"Connected": true})
 	})
 
+	r.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "Main website",
+		})
+	})
+
+	r.GET("/clicked", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"Connected": true})
+	})
 	fmt.Println("RUNNING")
 
-	r.Run()
+	r.Run(":8080")
 }
